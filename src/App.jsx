c5d2481ac3e6982153d75af705fe42ee20a4da19
Tiksha06 <tikshanagpal11940@gmail.com>
2026-06-05 20:53:23 +0530

    React Cute Calculator project with layout and logic 🧮✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [displayValue, setDisplayValue] = useState('0')
 
+  const buttons = [
+    'C', 'CE', '%', '÷',
+    '7', '8', '9', 'x',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '='
+  ]
+
+  const handleButtonClick = (value) => {
+      // Handle button click logic here
+      if (value === 'C') {
+        setDisplayValue('0');
+       } else if (value === 'CE') {
+        setDisplayValue('0');
+       } else if (value === '=') {
+        try {
+          let equation = displayValue.replaceAll('÷', '/').replaceAll('x', '*');
+          let result = eval(equation);
+          setDisplayValue(String(result));   //final result turn into String
+        } catch (error) {
+          setDisplayValue('Error'); // if equation is 5++2 [invalid]
+        }
+      }
+        else {
+          setDisplayValue((prev) => (prev === '0' ? value : prev + value));
+
+       }
+ }
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className= "calculator-container">
 
-      <div className="ticks"></div>
+        {/*** Lavender Box ***/}
+        <div className= "calculator-body">
+          <h2 className= "calculator-title"> 🧮 Cute Calculator </h2>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          {/* Screen Display */}
+          <div className= "calculator-screen">{displayValue}</div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+          {/* Buttons Grid */}
+          <div className= "button-grid">
+            {buttons.map((btn,index) => (
+              <button key = {index} className= "calc-button" onClick={() => handleButtonClick(btn)}>{btn}</button>
+            ))}
+            </div>
+          </div>
+        </div>
   )
 }
 
